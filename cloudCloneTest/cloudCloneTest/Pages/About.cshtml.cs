22a5feba98e87481b7fe4e8ebcb8d4aa2bf84bbe
PandeyS <PandeyS@CH4T4GMS827>
2019-04-06 18:17:32 +0530

    update about page

--- cloudCloneTest/cloudCloneTest/Pages/About.cshtml.cs
+++ cloudCloneTest/cloudCloneTest/Pages/About.cshtml.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace cloudCloneTest.Pages
 {
     public class AboutModel : PageModel
     {
+        private IConfiguration configuration;
+        public AboutModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+
+        }
         public string Message { get; set; }
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = this.configuration["greeting"];
         }
     }
 }
